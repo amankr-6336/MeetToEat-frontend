82c453572b5aad3cd8c51ba13c7840cf276bcaa7
amankr.6336 <amankr.17709418@gmail.com>
2023-10-01 21:50:33 +0530

    logo update

--- src/utils/AxiosClient.js
+++ src/utils/AxiosClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosClient=axios.create({
-    baseURL:"https://mte-b.onrender.com/",
+    baseURL:"http://localhost:4000",
     withCredentials:true,
 })
